efb4a873c5a74819969ab00808d82b0bae7c533c
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-11 11:07:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# deli_meals
+
+Flutter Meals app with several screens, navigations, filtering and data transfers.
---
+++ android/app/src/main/kotlin/com/w180/delimeals/deli_meals/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.w180.delimeals.deli_meals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
