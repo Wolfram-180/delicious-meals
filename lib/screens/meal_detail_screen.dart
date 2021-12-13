6a24d5f5b8b670bfe6468807b3b724098e458dc5
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-13 23:59:52 +0300

    added default route generation

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:deli_meals/data/dummy-data.dart';
 import 'package:flutter/material.dart';
 
 class MealDetailScreen extends StatelessWidget {
@@ -6,10 +7,48 @@ class MealDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context)?.settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
-      appBar: AppBar(title: Text('$mealId')),
-      body: Center(
-        child: Text('The meal - $mealId !'),
+      appBar: AppBar(title: Text('${selectedMeal.title}')),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(selectedMeal.imageUrl, fit: BoxFit.cover),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: Colors.grey,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
+            height: 200,
+            width: 300,
+            child: ListView.builder(
+              itemCount: selectedMeal.ingredients.length,
+              itemBuilder: (ctx, index) => Card(
+                color: Theme.of(context).colorScheme.secondary,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                  child: Text(selectedMeal.ingredients[index]),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
