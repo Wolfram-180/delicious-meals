e018ba17775a7fb016548bb7b189f064fee7d07b
Wolfram-180 <sergei.ulvis@gmail.com>
2023-10-09 15:55:59 +0300

    upd

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ class _MyAppState extends State<MyApp> {
       title: 'DeliMeals',
       theme: ThemeData(
         primarySwatch: Colors.pink,
-        accentColor: Colors.amber,
+        hintColor: Colors.amber,
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
